b5b9f09219b98d134c12790583d410f790c8a43d
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-10-24 10:48:56 +0530

    Character classes are special patterns in regular expressions that match specific sets of characters. Instead of matching a single literal character (like a or 3), a character class matches any character from a defined group. This is a support for the digit (\d) character class .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,47 @@
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
-    if (args.length != 2 || !args[0].equals("-E")) {
-      System.out.println("Usage: ./your_program.sh -E <pattern>");
-      System.exit(1);
+    public static void main(String[] args) {
+        if (args.length != 2 || !args[0].equals("-E")) {
+            System.out.println("Usage: ./your_program.sh -E <pattern>");
+            System.exit(1);
+        }
+
+        List<String> test = new ArrayList<>();
+
+        String pattern = args[1];
+        Scanner scanner = new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+
+        if (handlePattern(inputLine, pattern)) {
+            System.exit(0);
+        } else {
+            System.exit(1);
+        }
     }
 
-    String pattern = args[1];  
-    Scanner scanner = new Scanner(System.in);
-    String inputLine = scanner.nextLine();
+    public static boolean handlePattern(String inputLine, String pattern) {
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+        if (pattern.equals("\\d")) {
+            return handleDigitalCharacterClass(inputLine);
+        }
+        if (pattern.length() == 1) {
+            return handleSingleLetterPattern(inputLine, pattern);
+        }
 
-    // Uncomment this block to pass the first stage
-    // 
-     if (matchPattern(inputLine, pattern)) {
-         System.exit(0);
-     } else {
-         System.exit(1);
-     }
-  }
-
-  public static boolean matchPattern(String inputLine, String pattern) {
-    if (pattern.length() == 1) {
-      return inputLine.contains(pattern);
-    } else {
-      throw new RuntimeException("Unhandled pattern: " + pattern);
+        throw new RuntimeException("Unhandled pattern: " + pattern);
     }
-  }
+
+    public static boolean handleDigitalCharacterClass(String inputLine) {
+        final List<String> digits = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        return digits.stream().anyMatch(inputLine::contains);
+    }
+
+    public static boolean handleSingleLetterPattern(String inputLine, String pattern) {
+        return inputLine.contains(pattern);
+    }
+
 }
